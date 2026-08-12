352779c527f2edacb2fcd86b98210d190b8e1a5a
rizkyramadhani10 <rizkyramadhani1090@gmail.com>
2026-08-12 22:32:59 +0700

    Add prologue and dynamics menu background

---
+++ systems/cutscenes/prologue_screen.gd
@@ -0,0 +1,66 @@
+extends Control
+
+@onready var video_player = $VideoStreamPlayer
+@onready var skip_hint = $SkipHintLabel
+
+var tap_count = 0
+var fade_tween: Tween
+
+# Add a variable to track the exact millisecond of the last tap
+var last_tap_time: int = 0 
+
+func _ready():
+	skip_hint.modulate.a = 0.0
+	skip_hint.show() 
+	
+	video_player.finished.connect(_on_video_finished)
+	video_player.play()
+
+func _input(event):
+	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.pressed:
+		# Get the current time in milliseconds
+		var current_time = Time.get_ticks_msec()
+		
+		# Only process the tap if at least 100 milliseconds have passed since the last one
+		if current_time - last_tap_time > 100:
+			last_tap_time = current_time
+			_handle_tap()
+
+func _handle_tap():
+	tap_count += 1
+	
+	if tap_count == 1:
+		fade_in_hint()
+		_start_reset_timer()
+	elif tap_count >= 2:
+		finish_prologue()
+
+func fade_in_hint():
+	if fade_tween and fade_tween.is_valid():
+		fade_tween.kill()
+		
+	fade_tween = create_tween()
+	fade_tween.tween_property(skip_hint, "modulate:a", 1.0, 0.3)
+
+func _start_reset_timer():
+	var timer = get_tree().create_timer(3.0)
+	timer.timeout.connect(_on_timer_timeout)
+
+func _on_timer_timeout():
+	if tap_count == 1:
+		fade_out_hint()
+		tap_count = 0 
+
+func fade_out_hint():
+	if fade_tween and fade_tween.is_valid():
+		fade_tween.kill()
+		
+	fade_tween = create_tween()
+	fade_tween.tween_property(skip_hint, "modulate:a", 0.0, 0.5)
+
+func _on_video_finished():
+	finish_prologue()
+
+func finish_prologue():
+	video_player.stop()
+	TransitionScreen.transition_to_scene("res://levels/thirdLab(TimeMachine)/game.tscn")
--- systems/ui/main_menu/main_menu.gd
+++ systems/ui/main_menu/main_menu.gd
@@ -1,8 +1,17 @@
 extends Control
 
+@onready var background_rect = $TextureRect
 
+var default_bg = preload("res://systems/ui/main_menu/mainMenuBg/default_menu_bg.png")
+var completed_bg = preload("res://systems/ui/main_menu/mainMenuBg/completed_menu_bg.png")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if Global.is_game_completed:
+		# If they beat the game, show the special background
+		background_rect.texture = completed_bg
+	else:
+		# Otherwise, show the normal one
+		background_rect.texture = default_bg
 	pass # Replace with function body.
 
 
@@ -14,7 +23,7 @@ func _process(delta: float) -> void:
 
 func _on_play_button_pressed() -> void:
 	# This calls our global manager, fades out, switches the scene, and fades back in!
-	TransitionScreen.transition_to_scene("res://levels/thirdLab(TimeMachine)/game.tscn")
+	TransitionScreen.transition_to_scene("res://systems/cutscenes/prologue_screen.tscn")
 
 
 func _on_setting_button_pressed() -> void:
